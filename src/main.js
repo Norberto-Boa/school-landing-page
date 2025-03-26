bce58db3068866c8fc28013332e8f52619345102
Norberto Boa <nboa.26@gmail.com>
2025-03-26 23:47:15 +0200

    ADD: Responsive Navbar

--- src/main.js
+++ src/main.js
@@ -1,35 +1,19 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 28,
-  centeredSlides: false,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      centeredSlides: false,
-    },
-    568: {
-      slidesPerView: 2,
-      spaceBetween: 28,
-      centeredSlides: false,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 28,
-      centeredSlides: false,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 32,
-    },
-  },
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-collapse-toggle]").forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetId = button.getAttribute("data-collapse-toggle");
+      const target = document.getElementById(targetId);
+
+      console.log("clicked")
+      if (target) {
+        const isHidden = target.classList.contains("hidden");
+
+        // Toggle visibility
+        target.classList.toggle("hidden");
+
+        // Update ARIA attribute
+        button.setAttribute("aria-expanded", !isHidden);
+      }
+    });
+  });
 });
